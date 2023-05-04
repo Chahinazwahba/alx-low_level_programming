5e130fed50bb991b6d0b1d379380743d77248a91
chahinazwahba <chahinazwahba@gmail.com>
2023-05-04 15:42:36 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates two strings
+ * _strcat - function that concatenates
+ * two strings
  *
- * @dest: pointer to destnation input 
+ * @dest: pointer to destnation input
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
