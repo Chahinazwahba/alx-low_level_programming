b0ec360c0590ee5d3e577f1b4456d5eafa63d367
chahinazwahba <chahinazwahba@gmail.com>
2023-05-12 15:35:39 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - Printf program name, followed by a row line
+ * @argc: int
+ * @argv: List
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s/n", argc[0]);
+	return (0);
+}
