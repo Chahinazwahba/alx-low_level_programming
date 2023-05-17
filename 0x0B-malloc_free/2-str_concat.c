90997ce41cc41be4e626960b020f6e251072729c
chahinazwahba <chahinazwahba@gmail.com>
2023-05-16 17:18:59 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,14 @@
  * Return: pointer to the new string created (Success), or NULL (Error)
  */
 
-char *str_concat(char *size1, char *size2)
+char *str_concat(char *s1, char *s2)
 {
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (size1 && size1[len1])
+	while (s1 && s1[len1])
 		len1++;
-	while (size2 && size2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -26,20 +26,20 @@ char *str_concat(char *size1, char *size2)
 	i = 0;
 	j = 0;
 
-	if (size1)
+	if (s1)
 	{
 		while (i < len1)
 		{
-			s3[i] = size1[i];
+			s3[i] = s1[i];
 			i++;
 		}
 	}
 
-	if (size2)
+	if (s2)
 	{
 		while (i < (len1 + len2))
 		{
-			s3[i] = size2[j];
+			s3[i] = s2[j];
 			i++;
 			j++;
 		}
